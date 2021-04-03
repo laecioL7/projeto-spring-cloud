2eb60c363e8ca8a7cbb8e2286be58d357611f072
laecioL7 <laeciosousa07@outlook.com>
2021-04-03 16:34:36 -0300

    - criação do projeto api-gateway com zuul

---
+++ hr-api-gateway-zuul/src/test/java/com/example/hrapigateway/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hrapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
