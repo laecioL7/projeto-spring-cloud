58023e9798f83b033e54a88898a4a6c2007f93fe
laecioL7 <laeciosousa07@outlook.com>
2021-04-17 15:16:43 -0300

    Configs gerais de autorização e config server

--- hr-api-gateway-zuul/src/main/java/com/example/hrapigateway/config/AppConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/example/hrapigateway/config/AppConfig.java
@@ -1,23 +1,21 @@
-package com.example.hroauth.config;
+package com.example.hrapigateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 public class AppConfig {
-
-	@Bean
-	public BCryptPasswordEncoder bCryptPasswordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
+	
+	@Value("${jwt.secret}")
+	private String jwtSecret;
+	
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-		tokenConverter.setSigningKey("MY-SECRET-KEY");
+		tokenConverter.setSigningKey(jwtSecret);
 		return tokenConverter;
 	}
 
